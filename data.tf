d5929e58c8458bdad44f2dc3d2286c4f2d5b0a50
Joe Fecht <joe.fecht@ahead.com>
2023-06-01 13:14:02 -0500

    Added RBAC assignment

--- data.tf
+++ data.tf
@@ -1,1 +1,5 @@
 data "azurerm_client_config" "current" {}
+
+data "azurerm_role_definition" "kv_admin" {
+  name = "Key Vault Administrator"
+}
--- main.tf
+++ main.tf
@@ -1,27 +1,33 @@
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "random_password" "example" {
+resource "random_password" "pwd" {
   length  = 16
   special = true
 }
 
-resource "azurerm_key_vault" "example" {
-  name                        = var.key_vault_name
-  location                    = azurerm_resource_group.example.location
-  resource_group_name         = azurerm_resource_group.example.name
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  sku_name                    = "standard"
-  purge_protection_enabled    = true
+resource "azurerm_key_vault" "kv" {
+  name                     = var.key_vault_name
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  tenant_id                = data.azurerm_client_config.current.tenant_id
+  sku_name                 = "standard"
+  purge_protection_enabled = true
 }
 
-resource "azurerm_key_vault_secret" "example" {
+resource "azurerm_role_assignment" "kv_admin" {
+  scope                = azurerm_key_vault.kv.id
+  role_definition_name = data.azurerm_role_definition.kv_admin.name
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
+resource "azurerm_key_vault_secret" "secret" {
   name         = var.secret_name
-  value        = random_password.example.result
-  key_vault_id = azurerm_key_vault.example.id
+  value        = random_password.pwd.result
+  key_vault_id = azurerm_key_vault.kv.id
   depends_on = [
-    azurerm_key_vault.example
+    azurerm_key_vault.kv
   ]
 }

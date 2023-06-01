35b1dce95e0910ecdde65524b84bf8fb41754a3d
Joe Fecht <joe.fecht@ahead.com>
2023-06-01 13:31:22 -0500

    Testing with tags

--- data.tf
+++ data.tf
@@ -1,8 +1,10 @@
 data "azurerm_client_config" "current" {}
 
 data "azurerm_role_definition" "kv_admin" {
-  name = "Key Vault Administrator"
+  name = "Key Vault Secrets Officer"
 }
+
+
 
 data "azuread_user" "joe" {
   user_principal_name = "joe.fecht@copilotms.com"
